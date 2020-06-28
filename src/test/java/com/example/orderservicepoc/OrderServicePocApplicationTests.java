02132e7d6a1bd1e907778f49305604209e102606
Sercan Bayındır <sercan2256@gmail.com>
2020-06-28 13:15:20 +0300

    order-service spring boot application initialized from spring initializr
    Signed-off-by: Sercan Bayındır <sercan.bayindir@kgteknoloji.com>

---
+++ src/test/java/com/example/orderservicepoc/OrderServicePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.orderservicepoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServicePocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
